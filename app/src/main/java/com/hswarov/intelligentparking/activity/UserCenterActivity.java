853d8a735571fd8edbc146af3407311152b2b798
hswarov <hswarov@gmail.com>
2015-08-23 23:50:07 +0800

    completed the exit_dialog

--- app/src/main/java/com/hswarov/intelligentparking/activity/UserCenterActivity.java
+++ app/src/main/java/com/hswarov/intelligentparking/activity/UserCenterActivity.java
@@ -1,15 +1,18 @@
 package com.hswarov.intelligentparking.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 import com.hswarov.intelligentparking.MainActivity;
 import com.hswarov.intelligentparking.R;
 import com.hswarov.intelligentparking.app.BaseActivity;
+import com.hswarov.intelligentparking.widget.MyDialog;
 
 public class UserCenterActivity extends BaseActivity {
 
@@ -18,6 +21,7 @@ public class UserCenterActivity extends BaseActivity {
     private ImageButton ibMsgb;
     private ImageButton ibMsgw;
     private ScrollView mScrollview;
+    private RelativeLayout rlExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,31 @@ public class UserCenterActivity extends BaseActivity {
                 backToMainActivity();
             }
         });
+        rlExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertExitDialog();
+            }
+        });
+    }
+
+    private void alertExitDialog() {
+        MyDialog.Builder builder = new MyDialog.Builder(this);
+        builder.setMessage(R.string.dialog_text);
+        builder.setTitle(R.string.dialog_title);
+        builder.setPositiveButton(R.string.dialog_determine, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
     }
 
     private void backToMainActivity() {
@@ -53,6 +82,7 @@ public class UserCenterActivity extends BaseActivity {
         ibBackw = (ImageButton) findViewById(R.id.usercenter_back_white_ib);
         ibMsgb = (ImageButton) findViewById(R.id.usercenter_message_black_ib);
         ibMsgw = (ImageButton) findViewById(R.id.usercenter_message_white_ib);
+        rlExit= (RelativeLayout) findViewById(R.id.usercenter_exit_rl);
         mScrollview= (ScrollView) findViewById(R.id.usercenter_sv);
     }
 
