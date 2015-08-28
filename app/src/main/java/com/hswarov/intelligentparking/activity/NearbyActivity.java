c803f1a2f4a2fa4349e3af13e03e1e8aa20ffc9f
hswarov <hswarov@gmail.com>
2015-08-28 22:54:18 +0800

    do some preparation for navigation

--- app/src/main/java/com/hswarov/intelligentparking/activity/NearbyActivity.java
+++ app/src/main/java/com/hswarov/intelligentparking/activity/NearbyActivity.java
@@ -1,13 +1,11 @@
 package com.hswarov.intelligentparking.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.hswarov.intelligentparking.R;
+import com.hswarov.intelligentparking.app.BaseActivity;
 
-public class NearbyActivity extends AppCompatActivity {
+public class NearbyActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +13,5 @@ public class NearbyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nearby);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_nearby, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
