c86aa80946882d22835691f2b1f1a8f929d248a9
hswarov <hswarov@gmail.com>
2015-08-30 23:00:23 +0800

    add some code for nearbyActivity

--- app/src/main/java/com/hswarov/intelligentparking/activity/NearbyActivity.java
+++ app/src/main/java/com/hswarov/intelligentparking/activity/NearbyActivity.java
@@ -5,12 +5,24 @@ import android.os.Bundle;
 import com.hswarov.intelligentparking.R;
 import com.hswarov.intelligentparking.app.BaseActivity;
 
+import java.util.List;
+
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
+
 public class NearbyActivity extends BaseActivity {
+
+    private AutoScrollViewPager mViewPager;
+    private List<Integer> imageIdList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby);
+        initView();
+    }
+
+    private void initView() {
+        mViewPager = (AutoScrollViewPager) findViewById(R.id.nearby_view_pager);
     }
 
 
